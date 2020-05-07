337983f8ef933eb402211acf56d80e47ac0d9501
Jiri Majer <jiri.majer@eli-beams.eu>
2020-05-07 16:56:57 +0200

    cleanup after testing dummy jupyter for demo and not settingup ignores correctly

--- jupyter/sdf/utilities/CMakeFiles/sdfpy.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sdfpy"
-  "build/timestamp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sdfpy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
