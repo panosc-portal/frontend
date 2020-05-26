50a1dc4edf435c2fc4d8dbf936b0229afd978c1f
Jiri Majer <jiri.majer@eli-beams.eu>
2020-05-26 17:27:37 +0200

    all/single queries work

--- express/src/index.js
+++ express/src/index.js
@@ -1,14 +1,12 @@
-import 'dotenv/config';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import express from 'express';
-import models, {
-  connectDb
-} from './models/index.js';
-import routes from './routes';
-import jwt from 'jsonwebtoken'
+import "dotenv/config";
+import cors from "cors";
+import bodyParser from "body-parser";
+import express from "express";
+import models, { connectDb } from "./models/index.js";
+import routes from "./routes";
+import jwt from "jsonwebtoken";
 
-import data from './db'
+import data from "./db";
 
 const app = express();
 
@@ -17,14 +15,16 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(async (req, res, next) => {
   const token = req.header("token");
-  if (req.path === '/auth') return next();
-  if (!token) return res.sendStatus(403);
+  if (req.path === "/auth") return next();
+  // if (!token) return res.sendStatus(403);
 
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET);
@@ -34,22 +34,22 @@ app.use(async (req, res, next) => {
   } catch (e) {
     res.sendStatus(403);
   }
-})
+});
 
 app.use(async (req, res, next) => {
   req.context = {
     models,
   };
-  next()
+  next();
 });
 
 // Routes
 
-app.use('/users', routes.user);
-app.use('/documents', routes.document);
-app.use('/instances', routes.instance);
-app.use('/auth', routes.auth);
-app.use('/flavours', routes.flavour);
+app.use("/users", routes.user);
+app.use("/documents", routes.document);
+app.use("/instances", routes.instance);
+app.use("/auth", routes.auth);
+app.use("/flavours", routes.flavour);
 
 // Start
 
@@ -72,17 +72,16 @@ connectDb().then(async () => {
 
 app.listen(process.env.PORT);
 
-
 const createUsers = async () => {
   const admin = new models.User({
-    username: 'admin',
-    role: 'admin',
-    password: 'pass'
+    username: "admin",
+    role: "admin",
+    password: "pass",
   });
 
   const user = new models.User({
-    username: 'user',
-    password: 'pass'
+    username: "user",
+    password: "pass",
   });
 
   await admin.save();
@@ -91,30 +90,30 @@ const createUsers = async () => {
 
 const createFlavours = async () => {
   for (const flavour of data.Flavours) {
-    const newFlavour = models.Flavour
-    await newFlavour.create(flavour)
+    const newFlavour = models.Flavour;
+    await newFlavour.create(flavour);
   }
-}
+};
 const createInstances = async () => {
   for (const instance of data.Instances) {
     const flavour = await models.Flavour.findOne({
-      name: instance.flavour.name
-    })
-    instance.flavour = flavour
+      name: instance.flavour.name,
+    });
+    instance.flavour = flavour;
     instance.user = await models.User.findOne({
-      username: 'admin'
-    })
-    await models.Instance.create(instance)
+      username: "admin",
+    });
+    await models.Instance.create(instance);
   }
-}
+};
 const createDatasets = async () => {
   for (const dataset of data.Datasets) {
-    await models.Dataset.create(dataset)
+    await models.Dataset.create(dataset);
   }
-}
+};
 const createDocuments = async () => {
   for (const document of data.Documents) {
     document.datasets = await models.Dataset.find();
-    await models.Document.create(document)
+    await models.Document.create(document);
   }
-}
+};
