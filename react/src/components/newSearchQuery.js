af3663ff0b12c2c51cb6c06cf3669f9604d7aedb
Jiri Majer <jiri.majer@eli-beams.eu>
2020-06-05 08:48:51 +0200

    added a sample notebook for the demo..

--- react/src/components/newSearchQuery.js
+++ react/src/components/newSearchQuery.js
@@ -3,13 +3,18 @@ import ReactSlider from 'react-slider'
 import styled from 'styled-components'
 import {useForm, Controller} from 'react-hook-form'
 
-const removeEmpty = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    if (obj[key] && typeof obj[key] === 'object') removeEmpty(obj[key])
-    else if (obj[key] === undefined) delete obj[key]
-  })
-  return obj
-}
+// const removeEmpty = (obj) => {
+//   Object.keys(obj).forEach((key) => {
+//     if (obj[key] && typeof obj[key] === 'object') removeEmpty(obj[key])
+//     else if (obj[key] === undefined) delete obj[key]
+//   })
+//   return obj
+// }
+
+const listOfTechniques = [
+	"nadion thrust assembly",
+	"deuterium flow regulator"
+]
 
 const SearchQuery = ({setQuery}) => {
   const {register, handleSubmit, control} = useForm()
@@ -33,33 +38,39 @@ const SearchQuery = ({setQuery}) => {
       }
     ]
   }
+
+	const techniques = () => (<ListTechniques>
+		{	listOfTechniques.map(t => (<li key={t}> 
+			<input type="radio" name="technique" id={t} value={t} />
+			<label htmlFor={t}>{t}</label>
+		</li>))}
+	</ListTechniques>)
+	
   const submit = (data) => {
 	  const query = baseQuery
     data.title && (query.where = {title: {ilike: data.title}})
-    data.wavelength && query['include'].push({
-      relation: 'parameters',
-      scope: {
-        where: {
-          and: [
-            {name: 'wavelength'},
-            {value: {between: data.wavelength}},
-            {unit: 'nm'}
-          ]
-        }
-      }
-    })
+    // data.wavelength && query['include'].push({
+    //   relation: 'parameters',
+    //   scope: {
+    //     where: {
+    //       and: [
+    //         {name: 'wavelength'},
+    //         {value: {between: data.wavelength}},
+    //         {unit: 'nm'}
+    //       ]
+    //     }
+    //   }
+    // })
     setQuery(query)
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit(submit)}>
-        Title: <input type="text" ref={register} name="title" />
-        <br />
-        <Controller
+       <p>Title: <input type="text" ref={register} name="title" /></p>
+        <p><Controller
           as={
             <StyledSlider
-              // onAfterChange={val => console.log('onAfterChange value:', val)}
               defaultValue={[0, 2000]}
               min={0}
               max={2000}
@@ -71,10 +82,11 @@ const SearchQuery = ({setQuery}) => {
           }
           name="wavelength"
           control={control}
-        />
-        <input type="submit" value="Search" />
+        /></p>
+	 <p>{techniques()}</p>
+       <p><input type="submit" value="Search" /></p>
       </form>
-    </div>
+    st</div>
   )
 }
 
@@ -97,7 +109,9 @@ const StyledThumb = styled.div`
 `
 
 const Thumb = (props, state) => (
-  <StyledThumb {...props}>{state.valueNow}</StyledThumb>
+  <StyledThumb {...props}>
+	 {/*state.valueNow*/}
+	</StyledThumb>
 )
 
 const StyledTrack = styled.div`
@@ -105,6 +119,12 @@ const StyledTrack = styled.div`
   bottom: 40%;
   background: ${(props) =>
     props.index === 2 ? '#ddd' : props.index === 1 ? '#0f0' : '#ddd'};
+`
+
+const ListTechniques = styled.ul`
+list-style-type: none;
+margin: 0;
+padding:0;
 `
 
 const Track = (props, state) => <StyledTrack {...props} index={state.index} />
