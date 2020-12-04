6c0c57ed86d39f20bf8bdcb7c2f657ca68b34472
Jiri Majer <jiri.majer@eli-beams.eu>
2020-12-04 16:35:23 +0100

    single document cleanup

--- src/Documents/document.js
+++ src/Documents/document.js
@@ -7,6 +7,13 @@ import {parseDate} from '../App/helpers'
 import Spinner from '../App/spinner'
 import {Box, Flex, Heading, Link, Image, Text} from '../Primitives'
 
+const MetaItem = ({title, data}) => (
+  <S.MetaItem>
+    <Text fontWeight="bold">{title}</Text>
+    <Text>{data}</Text>
+  </S.MetaItem>
+)
+
 const Member = ({data}) => (
   <S.Tag>
     {data.person.fullName.substring(data.person.fullName.lastIndexOf(' ') + 1) +
@@ -14,12 +21,34 @@ const Member = ({data}) => (
       data.affiliation.name}
   </S.Tag>
 )
+const Members = ({members}) => (
+  <Flex>
+    {members.map(member => (
+      <Member key={member.id} data={member} />
+    ))}
+  </Flex>
+)
 
-const Detail = ({title, data}) => (
-  <S.Detail>
-    <Text fontWeight="bold">{title}</Text>
-    <Text>{data}</Text>
-  </S.Detail>
+const HeadingLink = ({title, pid}) => (
+  <Link as={RouterLink} to={'/documents/' + encodeURIComponent(pid)}>
+    <Heading>{title.substring(0, 90)}</Heading>
+  </Link>
+)
+
+const SummaryText = ({summary}) => {
+  return <Text>{summary.substring(0, 150)}...</Text>
+}
+
+const CitationLink = ({citation, doi}) => (
+  <Link href={'http://doi.org/' + doi}>{citation}</Link>
+)
+
+const Keywords = ({keywords}) => (
+  <Flex>
+    {keywords.map((keyword, index) => (
+      <S.Tag key={index}>{keyword}</S.Tag>
+    ))}
+  </Flex>
 )
 
 const Document = ({document, style}) =>
@@ -28,47 +57,29 @@ const Document = ({document, style}) =>
   ) : (
     <Box key={document.pid} style={style}>
       <S.Layout>
-        <S.Flex>
-          <Link
-            as={RouterLink}
-            to={'/documents/' + encodeURIComponent(document.pid)}
-          >
-            <Heading>{document.title.substring(0, 90)}</Heading>
-          </Link>
-
-          <Flex>
-            {document.members.map(member => (
-              <Member key={member.id} data={member} />
-            ))}
-          </Flex>
-
-          <Text>{document.summary.substring(0, 150)}...</Text>
-
-          <Link href={'http://doi.org/' + document.doi}>
-            {document.citation}
-          </Link>
-
-          <Flex>
-            {document.keywords.map((keyword, index) => (
-              <S.Tag key={index}>{keyword}</S.Tag>
-            ))}
-          </Flex>
-        </S.Flex>
-
-        <S.Details>
-          <Detail title="Type" data={document.type} />
-          <Detail
+        <S.Main>
+          <HeadingLink pid={document.pid} title={document.title} />
+          <Members members={document.members} />
+          <SummaryText summary={document.summary} />
+          <CitationLink citation={document.citation} doi={document.doi} />
+          <Keywords keywords={document.keywords} />
+        </S.Main>
+        <S.MetaList>
+          <MetaItem title="Type" data={document.type} />
+          <MetaItem
             title="Licence / Visibility"
             data={`${document.licence} / ${
               document.isPublic ? 'Public' : 'Non-Public'
             }`}
           />
-          <Detail title="Started on" data={parseDate(document.startDate)} />
-          <Detail title="Ended on" data={parseDate(document.endDate)} />
-          <Detail title="Released on" data={parseDate(document.releaseDate)} />
-        </S.Details>
-
-        <Image src={document.img} />
+          <MetaItem title="Started on" data={parseDate(document.startDate)} />
+          <MetaItem title="Ended on" data={parseDate(document.endDate)} />
+          <MetaItem
+            title="Released on"
+            data={parseDate(document.releaseDate)}
+          />
+        </S.MetaList>
+        <S.Image src={document.img} />
       </S.Layout>
     </Box>
   )
@@ -77,25 +88,35 @@ export default Document
 const S = {}
 S.Layout = styled(Box).attrs({
   display: 'grid',
-  marginBottom: [4],
-  sx: {gridGap: [1]},
+  marginBottom: [2, 1, 2, 3],
+  sx: {gridGap: '1px'},
 })`
-  grid-template-columns: 1fr max-content ${({theme}) => theme.sizes.image}px;
-  @media (max-width: ${({theme}) => theme.breakpoints[2]}) {
-    grid-template-columns: 220px 1fr;
-    grid-template-rows: 220px 300px;
-  }
+  grid-template-columns: 3fr 1fr 1fr;
 `
-S.Flex = styled(Flex).attrs({
+S.Main = styled(Flex).attrs({
   flexDirection: 'column',
   justifyContent: 'space-between',
   bg: 'middleground',
   p: 3,
 })``
-S.Details = styled(S.Flex).attrs({
+S.MetaList = styled(Flex).attrs({
   bg: 'background',
-  p: 0,
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  p: '1px',
 })``
+S.MetaItem = styled(Flex).attrs({
+  bg: 'middleground',
+  p: 1,
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+  marginBottom: '0.2rem',
+  flex: 'auto',
+})`
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`
 
 S.Tag = styled(Box).attrs({
   bg: 'foreground',
@@ -103,15 +124,4 @@ S.Tag = styled(Box).attrs({
   m: 1,
   marginLeft: 0,
 })``
-S.Detail = styled(Flex).attrs({
-  bg: 'middleground',
-  p: 1,
-  flexDirection: 'column',
-  justifyContent: 'space-evenly',
-  marginBottom: 1,
-  flex: 'auto',
-})`
-  &:last-of-type {
-    margin-bottom: 0;
-  }
-`
+S.Image = styled(Image).attrs()``
