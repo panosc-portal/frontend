6547fa3c69426534830d3a8556e2f99e52f9752b
Jiri Majer <jiri.majer@eli-beams.eu>
2021-01-12 14:08:24 +0100

    ramda playground, very messy

--- src/Search/search.js
+++ src/Search/search.js
@@ -1,145 +1,132 @@
-import React, {useState, useCallback} from 'react'
+import React, {useCallback} from 'react'
 
 import {Input} from '@rebass/forms'
 import styled from 'styled-components'
+import produce from 'immer'
 
 import debounce from 'lodash.debounce'
+import {baseQuery} from '../App/helpers'
 
-import {
-  mergeDeepWith,
-  concat,
-  compose,
-  omit,
-  isEmpty,
-  filter,
-  over,
-  lensProp,
-  assocPath,
-  dissocPath,
-} from 'ramda'
+import {isNil, concat, compose, mergeDeepWith, isEmpty, dissocPath} from 'ramda'
 
 import {mockTechniques} from '../App/helpers'
 import {useSearchStore} from '../App/stores'
-import {Box, Flex, Button, Card} from '../Primitives'
+import {Box, Flex, Button} from '../Primitives'
 
 const Search = () => {
-  const [
-    query,
-    setQuery,
-    resetQuery,
-    buttons,
-    setButtons,
-  ] = useSearchStore(state => [
+  const [query, setQuery, resetQuery] = useSearchStore(state => [
     state.query,
     state.setQuery,
     state.resetQuery,
-    state.buttons,
-    state.setButtons,
   ])
 
-  console.log(query)
+  const addTechnique = technique =>
+    produce(query.where?.keywords?.inq ?? [], draftTechniques => {
+      draftTechniques.push(technique)
+    })
 
-  const addTechnique = technique => {
-    const newT = {
-      ...query,
-      where: {
-        ...query.where,
-        keywords: {
-          inq: [technique],
-        },
-      },
-    }
+  const removeTechnique = technique =>
+    produce(query.where?.keywords?.inq ?? [], draftTechniques => {
+      draftTechniques.splice(
+        draftTechniques.findIndex(t => t === technique),
+        1
+      )
+    })
 
-    return mergeState(
-      query.where?.keywords?.inq
-        ? assocPath(
-            ['where', 'keywords', 'inq'],
-            [...query.where?.keywords?.inq, technique],
-            query
-          )
-        : newT
-    )
-  }
-
-  const removeTechnique = technique => ({
-    ...query,
-    where: {
-      ...query.where,
-      keywords: {inq: filter(s => s !== technique, query.where.keywords.inq)},
-    },
-  })
-
-  const addOrRemove = technique =>
+  const handleTechnique = technique =>
     query.where?.keywords?.inq.includes(technique)
       ? removeTechnique(technique)
       : addTechnique(technique)
 
-  const stripEmpty = obj =>
-    isEmpty(obj.where?.keywords?.inq)
-      ? over(lensProp('where'), omit(['keywords']), obj)
-      : obj
-
-  // const mergeState = newState => mergeDeepWith(concat, query, newState)
-  const mergeState = obj => obj
-  const updateTechniques = compose(setQuery, stripEmpty, addOrRemove)
-
-  const debounced = useCallback(
-    debounce(str => updateTitle(str), 500),
-    []
+  const debouncedTitleChange = debounce(
+    title => update(title, query.where?.keywords?.inq ?? []),
+    500
   )
 
-  const titleSearch = title => {
-    console.log(query.where)
-    const withTitle = {
-      ...query,
+  const handleTitle = e => debouncedTitleChange(e.target.value)
+
+  const isApplied = technique =>
+    query.where?.keywords?.inq.includes(technique) ? 'red' : 'text'
+
+  const stripTechniques = obj =>
+    isEmpty(obj.where?.keywords?.inq) || isNil(obj.where?.keywords?.inq)
+      ? dissocPath(['where', 'keywords'], obj)
+      : obj
+  const stripTitle = obj =>
+    isEmpty(obj.where?.title?.ilike) ? dissocPath(['where', 'title'], obj) : obj
+  const stripWhere = obj =>
+    isEmpty(obj.where?.title?.ilike) && isEmpty(obj.where?.keywords?.inq)
+      ? dissocPath(['where'], obj)
+      : obj
+
+  const whereQuery = useCallback((title, techniques) => {
+    const newQuery = {
       where: {
-        ...query.where,
-        title: {ilike: title},
+        keywords: {
+          inq: techniques,
+        },
+        title: {
+          ilike: title,
+        },
       },
     }
-    console.log(withTitle)
-    return mergeState(withTitle)
-  }
-  const stripEmptyTitle = obj =>
-    isEmpty(obj.where?.title?.ilike)
-      ? over(lensProp('where'), omit(['title']), obj)
-      : obj
-  const updateTitle = compose(setQuery, stripEmptyTitle, titleSearch)
+    return newQuery
+  }, [])
 
-  const onTitleInput = e => debounced(e.target.value)
-  const isApplied = technique =>
-    query.where?.keywords?.inq.includes(technique) ? 'background' : 'foreground'
+  const updateQuery = useCallback(
+    newQuery => {
+      const qq = mergeDeepWith(concat, newQuery, baseQuery)
+      setQuery(qq)
+    },
+    [setQuery]
+  )
+
+  const update = compose(
+    updateQuery,
+    stripWhere,
+    stripTechniques,
+    stripTitle,
+    whereQuery
+  )
 
   return (
-    <Box>
-      <Card>
-        <Box my={2}>
-          <Input
-            id="title"
-            placeholder="Search titles"
-            name="title"
-            defaultValue={query.where?.title?.ilike}
-            onChange={onTitleInput}
-          />
-        </Box>
-        <Flex
-          sx={{
-            flexDirection: 'column',
-            gap: 1,
-          }}
-        >
-          {mockTechniques.map((technique, index) => (
-            <S.Button
-              key={index}
-              bg={isApplied(technique)}
-              onClick={() => updateTechniques(technique)}
-            >
-              {technique}
-            </S.Button>
-          ))}
-        </Flex>
-      </Card>
-    </Box>
+    <Flex
+      sx={{
+        bg: 'middleground',
+        flexDirection: 'column',
+        gap: 3,
+      }}
+    >
+      <Input
+        id="title"
+        placeholder="Search titles"
+        name="title"
+        defaultValue={query.where?.title?.ilike}
+        onChange={handleTitle}
+      />
+      <Flex
+        sx={{
+          flexDirection: 'column',
+          gap: 1,
+        }}
+      >
+        {mockTechniques.map((technique, index) => (
+          <S.Button
+            key={index}
+            color={isApplied()}
+            onClick={() =>
+              update(
+                query.where?.title?.ilike ?? '',
+                handleTechnique(technique)
+              )
+            }
+          >
+            {technique}
+          </S.Button>
+        ))}
+      </Flex>
+      <S.Button onClick={() => resetQuery()}>Clear All</S.Button>
+    </Flex>
   )
 }
 
@@ -150,6 +137,9 @@ const S = {}
 S.Button = styled(Button).attrs({
   width: 1 / 1,
   sx: {
+    fontWeight: 400,
+    outline: 'none',
     textAlign: 'left',
+    bg: 'middleground',
   },
 })``
