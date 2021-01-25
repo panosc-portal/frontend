268084d82bacce4e6355cfc5d1912fe8fc3f06c8
Jiri Majer <jiri.majer@eli-beams.eu>
2021-01-25 11:59:01 +0100

    add some defaults to keycloak settings

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-  url: process.env.REACT_APP_KEYCLOAK_URL,
-  realm: process.env.REACT_APP_KEYCLOAK_REALM,
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+  url: process.env.REACT_APP_KEYCLOAK_URL ?? 'http://localhost:8080/auth',
+  realm: process.env.REACT_APP_KEYCLOAK_REALM ?? 'default',
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID ?? 'account',
 })
 
 export default keycloak
