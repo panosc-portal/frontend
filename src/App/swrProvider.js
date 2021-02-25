7cc6881c2615a978292d122e8edb9f6442ee7e8d
Jiří Majer <juras.majer@gmail.com>
2021-02-25 15:48:18 +0100

    Update swrProvider.js

--- src/App/swrProvider.js
+++ src/App/swrProvider.js
@@ -11,7 +11,7 @@ const SWRProvider = ({children}) => {
       ? keycloak.authenticated
         ? process.env.REACT_APP_API + url
         : false
-      : process.env.REACT_APP_SEARCH ?? 'http://localhost:5000/api' + url
+      : process.env.REACT_APP_SEARCH + url ?? 'http://localhost:5000/api' + url
   const fetcher = url => {
     console.log(getUrlWithBase(url))
     const access_token = keycloak?.token ?? ''
