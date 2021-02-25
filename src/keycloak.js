b1700b99c143665f4179e6bc0b93600085542263
Jiří Majer <juras.majer@gmail.com>
2021-02-25 15:15:35 +0100

    Update keycloak.js

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,9 +1,12 @@
 import Keycloak from 'keycloak-js'
+import {isEmpty} from 'ramda'
 
-const keycloak = new Keycloak({
-  url: process.env.REACT_APP_KEYCLOAK_URL ?? 'http://localhost:8080/auth',
-  realm: process.env.REACT_APP_KEYCLOAK_REALM ?? 'default',
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID ?? 'account',
-})
+const config = {
+  url: isEmpty(process.env.REACT_APP_KEYCLOAK_URL) ? 'http://localhost:8080/auth' : process.env.REACT_APP_KEYCLOAK_URL,
+  realm: isEmpty(process.env.REACT_APP_KEYCLOAK_REALM) ? 'default' : process.env.REACT_APP_KEYCLOAK_REALM,
+  clientId: isEmpty(process.env.REACT_APP_KEYCLOAK_CLIENT_ID) ? 'account' : process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+}
+
+const keycloak = new Keycloak(config)
 
 export default keycloak
